958fdb04b9df4b70259c92660c22dbee0f6bcc56
subhusri <subhusris@gmail.com>
2016-09-25 13:16:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function (req, res){
-    respond.send('aricle one is requested and will be served here')
+    respond.send('aricle one is requested and will be served here');
 });
 app.get('/article-two',function (req, res){
-    respond.send('aricle two is requested and will be served here')
+    respond.send('aricle two is requested and will be served here');
 });
 app.get('/article-three',function (req, res){
-    respond.send('aricle three is requested and will be served here')
+    respond.send('aricle three is requested and will be served here');
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
