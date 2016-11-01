5e60ffff396729cea59300b9a94f6a74db4ed129
subhusri <subhusris@gmail.com>
2016-11-01 14:52:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+
+
 var counter=0;
 app.get('/counter', function(req,res){
     counter=counter+1;
