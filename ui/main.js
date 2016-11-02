f833223614db9a187ca48b02937d938d11fc3874
subhusri <subhusris@gmail.com>
2016-11-02 11:56:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ button.onclick = function() {
          if(request.status ===200) {
              var counter = request.responseText;
              var span = document.getElementById('count');
-             span.InnerHTML = counter.toString();
+             span.innerHTML = counter.toString();
          }
      }
  };
